57fb59611922719e109e04a74eb05a801ce9949d
Omar123990 <oalash55@gmail.com>
2025-12-18 14:11:58 +0200

    buttonUp

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // ^ Write your JavaScript code here
+"use strict";
+
+
+const scrollBtn = document.getElementById("scroll-to-top");
+
+
+
+
+
+
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollBtn.classList.remove("opacity-0", "invisible");
+    scrollBtn.classList.add("opacity-100", "visible");
+  } else {
+    scrollBtn.classList.add("opacity-0", "invisible");
+    scrollBtn.classList.remove("opacity-100", "visible");
+  }
+});
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
